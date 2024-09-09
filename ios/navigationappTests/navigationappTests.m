1931f90f36d1133c5d4fab341278b05b3f8e7167
Nahuel De La Cruz <n_p4nic@hotmail.com>
2024-09-09 18:29:21 -0300

    Modificaciones para compatibilizar proyecto

--- ios/navigationappTests/navigationappTests.m
+++ ios/navigationappTests/navigationappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface navigationappTests : XCTestCase
+@interface NavigationAppTests : XCTestCase
 
 @end
 
-@implementation navigationappTests
+@implementation NavigationAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
